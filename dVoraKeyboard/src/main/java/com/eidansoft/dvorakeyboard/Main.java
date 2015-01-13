b3ed23208f7cf87b32de2d1c56949b6554fc34c0
Lorente, Alejandro <alejandro.lorente@sicpa.com>
2015-01-13 17:56:52 +0100

    Añado el inicio del proyecto dVoraKeyboard

---
+++ dVoraKeyboard/src/main/java/com/eidansoft/dvorakeyboard/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.eidansoft.dvorakeyboard;
+
+/**
+ *
+ * @author alorente
+ */
+public class Main {
+    public static void main(String[] args){
+        String cadenaAteclear = "asdflkj";
+        System.out.println(cadenaAteclear);
+//        for (int i=0; i<10; i++){
+//            System.out.print("\rPrueba " + i*10 + "%");
+//            try {
+//                Thread.sleep(1000);                 //1000 milliseconds is one second.
+//            } catch(InterruptedException ex) {
+//                Thread.currentThread().interrupt();
+//            }
+//        }
+    }
+}
