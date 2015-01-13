9b7e6f8c9edde146a80d874afb9c66b72a1ffa6f
Lorente, Alejandro <alejandro.lorente@sicpa.com>
2015-01-13 17:55:56 +0100

    Añado el proyecto original que me permite probar CDI pero sin maven, haciendo uso de la importacion manual de las dependencias

---
+++ CDITestSinMaven/src/com/eidansoft/cditestsinmaven/InjectedByDIImpl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.eidansoft.cditestsinmaven;
+
+/**
+ *
+ * @author alorente
+ */
+public class InjectedByDIImpl{
+
+    public void tryToExec() {
+        System.out.println(">>> begin tryToExec()");
+        System.out.println(">>> end tryToExec()");
+    }
+}
